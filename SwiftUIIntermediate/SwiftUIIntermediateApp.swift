172c8b1d9d256d86e8f84e8601f76971dd9c5107
linexy <linexy@naver.com>
2024-03-16 21:44:23 +0900

    Initial Commit

---
+++ SwiftUIIntermediate/SwiftUIIntermediateApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIIntermediateApp.swift
+//  SwiftUIIntermediate
+//
+//  Created by 김어진 on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntermediateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
